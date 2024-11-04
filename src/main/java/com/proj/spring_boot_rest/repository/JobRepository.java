f6f18db50cb2d3ac8abcb27f5bf72b8cb70ecd6f
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-11-04 10:36:20 +0300

    search action added to repo

--- src/main/java/com/proj/spring_boot_rest/repository/JobRepository.java
+++ src/main/java/com/proj/spring_boot_rest/repository/JobRepository.java
@@ -1,5 +1,7 @@
 package com.proj.spring_boot_rest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,44 +10,6 @@ import com.proj.spring_boot_rest.model.JobPost;
 @Repository
 public interface JobRepository extends JpaRepository<JobPost, Integer> {
 
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+
 }
-
-// // method to return all JobPosts
-// public List<JobPost> returnAllJobPosts() {
-// return jobs;
-// }
-
-// // method to save a job post object into arrayList
-// public void addJobPost(JobPost job) {
-// jobs.add(job);
-
-// }
-
-// public JobPost getJob(int postId) {
-// for (JobPost job : jobs) {
-// if (job.getPostId() == postId) {
-// return job;
-// }
-// }
-
-// return null;
-// }
-
-// public void updateJob(JobPost jobPost) {
-// for (JobPost jp : jobs) {
-// if (jp.getPostId() == jobPost.getPostId()) {
-// jp.setPostProfile(jobPost.getPostProfile());
-// jp.setPostDesc(jobPost.getPostDesc());
-// jp.setReqExperience(jobPost.getReqExperience());
-// jp.setPostTechStack(jobPost.getPostTechStack());
-// }
-// }
-// }
-
-// public void deleteJob(int postId) {
-// for (JobPost jp : jobs) {
-// if (jp.getPostId() == postId) {
-// jobs.remove(postId);
-// }
-// }
-// }
