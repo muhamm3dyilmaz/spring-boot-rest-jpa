87279cf4139410b0df046f343ab25ba2e8752cdc
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-11-18 14:34:10 +0300

    JoinPoint added to LoggingAspect

--- src/main/java/com/proj/spring_boot_rest/aop/LoggingAspect.java
+++ src/main/java/com/proj/spring_boot_rest/aop/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.proj.spring_boot_rest.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,9 +14,14 @@ public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
     // return type, class-name.method-name(args)
-    @Before("execution(* com.proj.spring_boot_rest.service.JobService.*(..))")
-    public void logMethodCall() {
-        LOGGER.info("Method Called");
+    // @Before("execution(* com.proj.spring_boot_rest.service.JobService.*(..))")
+    // public void logMethodCall() {
+    //     LOGGER.info("Method Called");
+    // }
+
+    @Before("execution(* com.proj.spring_boot_rest.service.JobService.getJob*(..))")
+    public void logMethodCall(JoinPoint jp) {
+        LOGGER.info("Method Called " + jp.getSignature().getName());
     }
 
 }
