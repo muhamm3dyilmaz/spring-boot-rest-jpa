f531f39ecc2f9e59389e41f7b01fba3ccbb0218c
muhamm3dyilmaz <mylmz2101@gmail.com>
2024-12-16 16:26:49 +0300

    user model created

---
+++ src/main/java/com/proj/spring_boot_rest/model/User.java
@@ -0,0 +1,16 @@
+package com.proj.spring_boot_rest.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private int Id;
+    private String username;
+    private String password;
+}
